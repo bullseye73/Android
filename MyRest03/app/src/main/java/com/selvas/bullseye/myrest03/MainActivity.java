bfd9a05a175bb4fa0f54c6a9cf64e18759cd70a1
bullseye <bullseye73@gmail.com>
2019-02-07 18:06:32 +0900

    Android http get basic source

---
+++ MyRest03/app/src/main/java/com/selvas/bullseye/myrest03/MainActivity.java
@@ -0,0 +1,73 @@
+package com.selvas.bullseye.myrest03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onRequestPost(View v){
+
+    }
+
+    public static String buildPostParameters(Object content) {
+        String output = null;
+        if ((content instanceof String) ||
+                (content instanceof JSONObject) ||
+                (content instanceof JSONArray)) {
+            output = content.toString();
+        } else if (content instanceof Map) {
+            Uri.Builder builder = new Uri.Builder();
+            HashMap hashMap = (HashMap) content;
+            if (hashMap != null) {
+                Iterator entries = hashMap.entrySet().iterator();
+                while (entries.hasNext()) {
+                    Map.Entry entry = (Map.Entry) entries.next();
+                    builder.appendQueryParameter(entry.getKey().toString(), entry.getValue().toString());
+                    entries.remove(); // avoids a ConcurrentModificationException
+                }
+                output = builder.build().getEncodedQuery();
+            }
+        }
+
+        return output;
+    }
+
+    public static URLConnection makeRequest(String method, String apiAddress, String accessToken, String mimeType, String requestBody) throws IOException {
+        URL url = new URL(apiAddress);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        urlConnection.setDoInput(true);
+        urlConnection.setDoOutput(!method.equals("GET"));
+        urlConnection.setRequestMethod(method);
+
+        urlConnection.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+        urlConnection.setRequestProperty("Content-Type", mimeType);
+        OutputStream outputStream = new BufferedOutputStream(urlConnection.getOutputStream());
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "utf-8"));
+        writer.write(requestBody);
+        writer.flush();
+        writer.close();
+        outputStream.close();
+
+        urlConnection.connect();
+
+        return urlConnection;
+    }
+}
